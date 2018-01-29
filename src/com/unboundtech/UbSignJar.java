b1053b32fcf28e0bf5b062a601e1987428beefe5
saar@unboundtech <saar.peer@dyadicsec.com>
2018-01-29 10:29:21 +0200

    Fix win and unix paths

--- src/com/unboundtech/UbSignJar.java
+++ src/com/unboundtech/UbSignJar.java
@@ -27,7 +27,8 @@ public class UbSignJar extends SignJar {
 
     private static final String PROVIDER_JAR_NAME = "ekm-java-provider-2.0.jar";
     private static final String WIN_PROVIDER_FOLDER = "lib";
-    private static final String WIN_DEFAULT_INSTALLATION_DIR = "C:\\Program Files\\DyadicSec";
+    private static final String WIN_CLIENT_INSTALLATION_DIR = "C:\\Program Files\\Dyadic\\ekm-client";
+    private static final String WIN_SERVER_INSTALLATION_DIR = "C:\\Program Files\\Dyadic\\ekm";
 
     private static final String UNIX_INSTALLATION_DIR  = "/";
     private static final String UNIX_PROVIDER_FOLDER = "usr/lib";
@@ -115,25 +116,70 @@ public class UbSignJar extends SignJar {
 
     /**
      *
+     * @param installationDir
+     * @param providerFolder
+     * @return
      */
-    private void resolveProviderPathOnWindows() {
-        if (installationDir == null) {
-            installationDir = WIN_DEFAULT_INSTALLATION_DIR;
-        }
-        providerPath = Paths.get(installationDir).resolve(WIN_PROVIDER_FOLDER).toAbsolutePath().toString();
+    private String resolveProviderFullPath(String installationDir, String providerFolder){
+        return resolveProviderFullPath(Paths.get(installationDir).resolve(providerFolder).toAbsolutePath().toString());
+    }
+
+    /**
+     *
+     * @param providerPath
+     * @return
+     */
+    private String resolveProviderFullPath(String providerPath){
+        return Paths.get(providerPath).resolve(PROVIDER_JAR_NAME).toAbsolutePath().toString();
     }
 
     /**
      *
      */
-    private void resolveProviderPathOnUnix() {
-        if (installationDir == null) {
-            installationDir = UNIX_INSTALLATION_DIR;
+    private String resolveProviderPathOnWindows() {
+
+        String installationDir;
+
+        // try with the client installation dir first
+        if (this.installationDir == null) {
+            installationDir = WIN_CLIENT_INSTALLATION_DIR;
+        } else {
+            installationDir = this.installationDir;
         }
 
-        providerPath = Paths.get(installationDir).resolve(UNIX_PROVIDER_FOLDER).toAbsolutePath().toString();
-        if(!new File(providerPath).exists())
-            providerPath = Paths.get(installationDir).resolve(UNIX_64_PROVIDER_FOLDER).toAbsolutePath().toString();
+        String providerFullPath = resolveProviderFullPath(installationDir, WIN_PROVIDER_FOLDER);
+
+        // try with the server installation dir if file is ot exists
+        if (!new File(providerFullPath).exists()) {
+            if (this.installationDir == null) {
+                installationDir = WIN_SERVER_INSTALLATION_DIR;
+                providerFullPath = resolveProviderFullPath(installationDir, WIN_PROVIDER_FOLDER);
+            }
+        }
+        return providerFullPath;
+    }
+
+    /**
+     *
+     */
+    private String resolveProviderPathOnUnix() {
+        String installationDir;
+
+        if (this.installationDir == null) {
+            installationDir = UNIX_INSTALLATION_DIR;
+        }
+        else {
+            installationDir = this.installationDir;
+        }
+
+        // try with usr/lib
+        String providerFullPath = resolveProviderFullPath(installationDir, UNIX_PROVIDER_FOLDER);
+
+        if(!new File(providerFullPath).exists())
+            // try with use/lib64
+            providerFullPath = resolveProviderFullPath(installationDir, UNIX_64_PROVIDER_FOLDER);
+
+        return providerFullPath;
 
     }
 
@@ -142,17 +188,18 @@ public class UbSignJar extends SignJar {
      */
     private void setClasspath(ExecTask cmd) {
 
+        String providerFullPath;
+
         if (providerPath == null) {
-            if (isWindows()) resolveProviderPathOnWindows();
-            else resolveProviderPathOnUnix();
+            providerFullPath = isWindows() ? resolveProviderPathOnWindows() : resolveProviderPathOnUnix();
+        } else {
+            providerFullPath = resolveProviderFullPath(providerPath);
         }
 
-        String providerFullPath = Paths.get(providerPath).resolve(PROVIDER_JAR_NAME).toAbsolutePath().toString();
+        log(String.format("Using provider JAR: %s", providerFullPath));
 
-        if(!new File(providerFullPath).exists())
-            throw new BuildException(String.format("Provider jar file (%s) does not exist",providerFullPath));
-
-        log(String.format("Using provider JAR: %s",providerFullPath));
+        if (!new File(providerFullPath).exists())
+            throw new BuildException(String.format("Provider jar file (%s) does not exist", providerFullPath));
 
         String classpath = System.getProperty("java.class.path");
 
@@ -195,4 +242,5 @@ public class UbSignJar extends SignJar {
     public static boolean isWindows() {
         return System.getProperty("os.name").startsWith("Windows");
     }
+    
 }
